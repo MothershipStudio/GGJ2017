ab85bb8be833f2aa9fbfaf647e71e8e40066badd
vagnerseibert <vagnerseibert@gmail.com>
2017-01-21 05:47:16 -0200

    ztargeting quase terminado

---
+++ GGJ2017/Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    private MagnetController magnetController;
+    private ZTargeter ztargeter;
+
+	void Start () {
+        magnetController = GetComponent<MagnetController>();
+        ztargeter = GetComponent<ZTargeter>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKeyDown(KeyCode.Z)) {
+            ztargeter.ai
+        }
+	}
+}
--- GGJ2017/Assets/Scripts/ZTargeter.cs
+++ GGJ2017/Assets/Scripts/ZTargeter.cs
@@ -4,24 +4,44 @@ using UnityEngine;
 
 public class ZTargeter : MonoBehaviour {
     public LayerMask targetMask;
-    Vector3 currentTarget;
-    SortedList<Vector3, float> targets = new SortedList<Vector3, float>();
+    public Rigidbody2D currentTarget { get; set; }
+    SortedList<float, Rigidbody2D> targets = new SortedList<float, Rigidbody2D>();
 
     Vector2 w_screen_size;
-
-        
+    int currentTargetIndex = 0;
 
     private void Start() {
         w_screen_size = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
     }
 
+    private void Update() {
+        if(Input.GetMouseButtonDown(0)) {
+            UpdateTargets();
+            foreach(var target in targets) {
+                Debug.Log(target.Value.name);
+            }
+        }
+    }
+
+    public void Aim() {
+        currentTarget = targets.Values[currentTargetIndex];
+        currentTargetIndex = (currentTargetIndex + 1) % targets.Count;
+
+    }
+
+    void Unaim() {
+        currentTargetIndex = 0;
+    }
+
     void UpdateTargets() {
         targets.Clear();
-        var hits = Physics2D.BoxCastAll(Camera.main.transform.position, w_screen_size, 0, Vector2.zero, targetMask);
+        var hits = Physics2D.BoxCastAll(Camera.main.transform.position, w_screen_size * 100, 0, Vector2.zero, 0, targetMask);
 
         foreach(var hit in hits) {
             var tpos = hit.transform.position;
-            targets.Add(tpos, Vector3.SqrMagnitude(tpos - transform.position));
+            Vector3 toTarget = tpos - transform.position;
+            int dirMask = (int)(-Mathf.Sign(transform.lossyScale.x * toTarget.x) + 1) << 16;
+            targets.Add(Vector3.SqrMagnitude(toTarget) + dirMask, hit.collider.GetComponent<Rigidbody2D>());
         }
     }
 }
