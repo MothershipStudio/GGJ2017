954090d97982bc97b591ab3c74241461ef66ac20
Andre Felipe dos Santos <andrefelipe.ecp@gmail.com>
2017-01-21 16:16:30 -0200

    Créditos
    adicionada cena de créditos

---
+++ GGJ2017/Assets/Scripts/Menu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour {
+
+    public Button start;
+    public Button credits;
+
+	// Use this for initialization
+	void Start () {
+        start = start.GetComponent<Button>();
+        credits = credits.GetComponent<Button>();
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene(3);
+    }
+}
