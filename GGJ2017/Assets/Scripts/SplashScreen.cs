6dfb62609bc727b3fc7098d4a9e4d04d5467eda7
Andre Felipe dos Santos <andrefelipe.ecp@gmail.com>
2017-01-21 09:08:55 -0200

    Criação da splash screen

---
+++ GGJ2017/Assets/Scripts/SplashScreen.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(AudioSource))]
+public class SplashScreen : MonoBehaviour
+{
+    public MovieTexture screenTexture;
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        GetComponent<RawImage>().texture = screenTexture as MovieTexture;
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = screenTexture.audioClip;
+        screenTexture.Play();
+        audioSource.Play();
+    }
+
+    void Update()
+    {
+        if (!screenTexture.isPlaying)
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
