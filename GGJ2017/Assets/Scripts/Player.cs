f7d4a2856bf68daf1cdd936c250e94e9624c3f1e
vagnerseibert <vagnerseibert@gmail.com>
2017-01-21 22:00:37 -0200

    Corrigido conflitos

--- GGJ2017/Assets/Scripts/Player.cs
+++ GGJ2017/Assets/Scripts/Player.cs
@@ -3,23 +3,57 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
+    public Transform plusHand;
+    public Transform minusHand;
     private MagnetController magnetController;
     private ZTargeter ztargeter;
 
-    public float KnockbackForce = 20f;
-    public int MaxLife = 100, CurrentLife = 100;
+    private bool applyingPositive;
+    private bool applyingNegative;
+    private ArmsTrigger[] armsTrigger;
+    private Rigidbody2D rb2d;
 
-	void Start () {
+    void Start() {
         magnetController = GetComponent<MagnetController>();
         ztargeter = GetComponent<ZTargeter>();
-	}
-	
-	void Update () {
+        armsTrigger = GetComponentsInChildren<ArmsTrigger>();
+        rb2d = GetComponent<Rigidbody2D>();
+
+        for(int i = 0; i < armsTrigger.Length; i++) {
+            armsTrigger[i].onTriggerEnter.AddListener(OnArmsTriggerEnter);
+            armsTrigger[i].GetComponent<CircleCollider2D>().enabled = false;
+        }
+    }
+
+    void SetArmsActivate(bool setActive) {
+        for(int i = 0; i < armsTrigger.Length; i++) {
+            armsTrigger[i].GetComponent<CircleCollider2D>().enabled = setActive;
+        }
+    }
+
+    void OnArmsTriggerEnter(Collider2D col, Transform dad) {
+        if(applyingPositive && ztargeter.currentTargetPositive.gameObject == col.gameObject && rb2d.mass > ztargeter.currentTargetPositive.mass) {
+            Debug.Log("OnArmsTriggerEnter: " + col.gameObject.name);
+            ztargeter.currentTargetPositive.velocity = Vector2.zero;
+            applyingPositive = false;
+            ztargeter.currentTargetPositive.gravityScale = 0;
+            col.transform.parent = (dad);
+        }
+    }
+
+    void Aim(Rigidbody2D oldTar, Rigidbody2D newTar) {
+        if(ztargeter.currentTargetPositive != null)
+            ztargeter.currentTargetPositive.GetComponent<Renderer>().material.SetColor("_OutlineColor", Color.clear);
+        ztargeter.AimPositive();
+        ztargeter.currentTargetPositive.GetComponent<Renderer>().material.SetColor("_OutlineColor", Color.black);
+    }
+
+    void Update() {
         if(Input.GetKeyDown(KeyCode.Q)) {
             if(ztargeter.currentTargetPositive != null)
                 ztargeter.currentTargetPositive.GetComponent<Renderer>().material.SetColor("_OutlineColor", Color.clear);
             ztargeter.AimPositive();
-            ztargeter.currentTargetPositive.GetComponent<Renderer>().material.SetColor("_OutlineColor", Color.red);
+            ztargeter.currentTargetPositive.GetComponent<Renderer>().material.SetColor("_OutlineColor", Color.black);
         }
 
         if(Input.GetKeyDown(KeyCode.E)) {
@@ -28,24 +62,43 @@ public class Player : MonoBehaviour {
             ztargeter.AimNegative();
             ztargeter.currentTargetNegative.GetComponent<Renderer>().material.SetColor("_OutlineColor", Color.blue);
         }
-    }
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        var op = collision.gameObject.GetComponent<ObjectProperties>();
+        if(Input.GetKeyDown(KeyCode.Z)) {
+            SetArmsActivate(true);
+            applyingPositive = true;
+        }
 
-        if (op != null) {
-            if (op.Damages == ObjectProperties.DamageOn.PlayerOnly || op.Damages == ObjectProperties.DamageOn.Both)
-            {
-                this.CurrentLife -= (int)(op.Damage);
-                if(this.CurrentLife <= 0)
-                {
-                    //TODO: funcao de morte
-                }
+        if(Input.GetKeyDown(KeyCode.C)) {
+            SetArmsActivate(true);
+            applyingNegative = true;
+        }
 
-                var knockback = (this.transform.position - collision.transform.position).normalized * KnockbackForce;
-                GetComponent<Rigidbody2D>().AddForce(knockback);
-            }
+        if(Input.GetKeyUp(KeyCode.Z)) {
+            SetArmsActivate(false);
+            applyingPositive = false;
+            magnetController.Release(ztargeter.currentTargetPositive);
+        }
+
+        if(Input.GetKeyUp(KeyCode.C)) {
+            SetArmsActivate(false);
+            applyingNegative = false;
+            magnetController.Release(ztargeter.currentTargetNegative);
         }
     }
+
+    private void FixedUpdate() {
+        if(applyingPositive) {
+            if((ztargeter.currentTargetPositive.position - (Vector2)plusHand.position).sqrMagnitude > 2 * magnetController.magnetMaxRange)
+                ztargeter.AimPositive();
+            magnetController.AplyMagnetForce(plusHand.position, ztargeter.currentTargetPositive, 1, 1);
+        }
+
+        if(applyingNegative) {
+            if((ztargeter.currentTargetNegative.position - (Vector2)plusHand.position).sqrMagnitude > 2 * magnetController.magnetMaxRange)
+                ztargeter.AimNegative();
+            magnetController.AplyMagnetForce(minusHand.position, ztargeter.currentTargetPositive, -1, 1);
+        }
+
+    }
+
 }
