cb503dd7c7cd57d94f791b481fef680ef38ebed8
Andre Felipe dos Santos <andrefelipe.ecp@gmail.com>
2017-01-22 07:55:18 -0200

    Add musica

---
+++ GGJ2017/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+
+    private AudioSource audioSource;
+    public AudioClip[] themes;
+    int index = 0;
+    // Use this for initialization
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = themes[index];
+        audioSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!audioSource.isPlaying)
+        {
+            index = (index + 1) % themes.Length;
+            audioSource.clip = themes[index];
+            audioSource.Play();
+        }
+    }
+}
